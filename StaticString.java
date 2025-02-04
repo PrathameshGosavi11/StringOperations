2d634345feb174ee48e136dbfabf0a1d8ab715ad
Prathamesh Gosavi <prathameshgosavi945@gmail.com>
2025-02-04 22:58:20 +0530

    Static and dyanamic String

---
+++ StaticString.java
@@ -0,0 +1,14 @@
+public class StaticString {
+    
+    public static void main(String[] args) {
+        
+        String str1="Java";
+        String str2="Java"; // Static String creation.
+
+        String str3=new String("Hello");
+        String str4=new String("World"); //dyanamic String creation.
+
+
+        
+    }
+}
