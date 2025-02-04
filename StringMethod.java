e5f7e8b5163d85fb5cc87d4da0fedb9917549b7e
Prathamesh Gosavi <prathameshgosavi945@gmail.com>
2025-02-04 23:43:18 +0530

    add files

---
+++ StringMethod.java
@@ -0,0 +1,15 @@
+public class StringMethod {
+    public static void main(String[] args) {
+        
+        String str1="hello java";
+        System.out.println("Before converting the cases:"+str1);
+        String result=str1.toUpperCase();
+        System.out.println("after apply the case conversion:"+result);
+
+        System.out.println("before conversion the lowercase:"+result);
+        System.out.println("after apply case conversion:"+result.toLowerCase());
+
+       
+
+    }
+}
