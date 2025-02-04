3c0d324dc2ad1130148b653642f74e59cb9c7727
Prathamesh Gosavi <prathameshgosavi945@gmail.com>
2025-02-04 15:19:04 +0530

    add String file

---
+++ StringInterfaces.java
@@ -0,0 +1,13 @@
+public class StringInterfaces
+{
+    public static void main(String[] args) {
+        
+        CharSequence ch="Hello world";
+
+        System.out.println("String  is:"+ch);
+
+        System.out.println("lenghth is:"+ch.length());
+
+        System.out.println("String is empty or not:"+ch.isEmpty());
+    }
+}
