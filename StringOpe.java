c0085401776d00cd3b2649a414f864177c3981f9
Prathamesh Gosavi <prathameshgosavi945@gmail.com>
2025-02-04 18:34:03 +0530

    added String == operation

---
+++ StringOpe.java
@@ -0,0 +1,18 @@
+public class StringOpe {
+
+    public static void main(String[] args) {
+        
+        String s1=new String("jay");
+
+        String s2=new String("jay");
+
+        if(s1==s2)
+        {
+            System.out.println("both are same");
+        }
+        else
+        {
+            System.out.println("not same");
+        }
+    }
+}
